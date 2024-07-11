9fe87a79f1c4e7e6782ca99d9b51c8f8e324e1a4
Bailey <baileycishk@gmail.com>
2024-07-10 21:58:51 -0400

    trial sqlite

--- vergil/production.py
+++ vergil/production.py
@@ -10,16 +10,16 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] # vergil.azurewebsites.net
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'mssql',
-        'PORT': '1433',
-        'NAME': os.environ['DB_NAME'],
-        'HOST': os.environ['DB_SERVER'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'OPTIONS': {
-                'driver': 'ODBC Driver 17 for SQL Server',
-            },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mssql',
+#         'PORT': '1433',
+#         'NAME': os.environ['DB_NAME'],
+#         'HOST': os.environ['DB_SERVER'],
+#         'USER': os.environ['DB_USER'],
+#         'PASSWORD': os.environ['DB_PASSWORD'],
+#         'OPTIONS': {
+#                 'driver': 'ODBC Driver 17 for SQL Server',
+#             },
+#     }
+# }
