5af9620c7d7ca90d41a5687110f1cfd0d1f444d8
Bailey <baileycishk@gmail.com>
2024-07-13 01:39:00 -0400

    Add Search Bar, Reformatted NavBar

--- bookcat/filters.py
+++ bookcat/filters.py
@@ -1,14 +1,30 @@
 import django_filters
-from django_filters import DateFilter
+from django_filters import DateFilter, ChoiceFilter
 from bookcat.models import Book
 
 class BookFilter(django_filters.FilterSet):
     start_date = DateFilter(field_name="date", lookup_expr="gte")
     end_date = DateFilter(field_name="date", lookup_expr="lte")
+    temp = ChoiceFilter(field_name="category", lookup_expr="icontains",
+                        choices=(
+                            ('Printed Copies of Codices', 'Printed Copies of Codices'),
+                            ('Incunabula', 'Incunabula'),
+                            ('Opera', 'Opera'),
+                            ('Opuscula', 'Opuscula'),
+                            ('Bucolica Et Georgica', 'Bucolica Et Georgica'),
+                            ('Eclogae', 'Eclogae'),
+                            ('Georgica', 'Georgica'),
+                            ('Aeneis', 'Aeneis'),
+                            ('Centones)', 'Centones'),
+                            ('Translations', 'Translations'),
+                            ('Travesties', 'Travesties'),
+                            ('The Bancroft Collection of English Versions of Virgil', 'The Bancroft Collection of English Versions of Virgil'),
+                            ('Works About Virgil And His Poems', 'Works About Virgil And His Poems'),
+                            ('', 'Any'),)
+                            )
     class Meta:
         model = Book
         fields = {'title_description': ['contains'],
                   'contributors': ['contains'],
-                  # 'category',
-                  'language': ['exact']
+                  'language': ['contains']
                 }
